98c33e164fc237b1e20c9e3f65f54b69e32bf502
jason <c17764691@mydit.ie>
2019-12-04 22:19:30 +0000

    fixed py

--- myapp/serve.py
+++ myapp/serve.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask import render_template
 
 # creates a Flask application called app
-app = Flask(_name_)
+app = Flask(__name__)
 
 # a route where we will display a welcome message via a HTML template
 @app.route("/")
